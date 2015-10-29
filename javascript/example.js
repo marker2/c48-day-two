314525262a99e799be19db8adf27cbbb2eccb80d
Josh Wall <joshua@onthecity.org>
2015-10-28 21:01:23 -0700

    my first commit

---
+++ javascript/example.js
@@ -0,0 +1,24 @@
+//alert("It worked!");
+//console.log("Where am I?");
+
+var someNumber = 12;
+var anotherNumber = someNumber + 4;
+var name = "Josh";
+var aLocation = "University of Washington";
+var quote = 'A great man once said, "Always do such and such."';
+
+console.log(someNumber);
+console.log(anotherNumber);
+console.log(quote);
+
+var firstFavFood = "pizza";
+var secondFavFood = "ice cream";
+
+var favoriteFoods = ["pizza", "ice cream", "burgers"]
+
+console.log(favoriteFoods);
+console.log(favoriteFoods[0]);
+console.log(favoriteFoods[1]);
+
+var thirdFav = favoriteFoods[2];
+console.log(thirdFav);
